276d0e939be947177037b3da70e6d9cc095c94d8
LJPomoxis <lukeottojack@gmail.com>
2026-04-05 21:51:20 -0600

    Updated renderer code with starts of code for minefield display

--- include/renderer/renderer.hpp
+++ include/renderer/renderer.hpp
@@ -1,11 +1,34 @@
 #pragma once
+#include <SFML/Window.hpp>
+
+enum class CellState { MINE, FLAG, HIDDEN, EMPTY, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT };
+
+class FieldDisplay : public sf::Drawable {
+private:
+    sf::VertexArray vertices;
+    sf::Texture spritesheet;
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+        states.texture = &spritesheet;
+        target.draw(vertices, states);
+    }
+
+public:
+    void setSpritesheet(std::string texPath);
+};
 
 class Renderer {
 private:
+    FieldDisplay fieldDisplay;
+    int buffer = 10;
+    int windowW;
+    int windowH;
 
 public:
-    Renderer();
+    Renderer(int w, int h);
 
-    void renderField();
-    void fieldToScreen();
+    void renderField(sf::Window &window);
+    void resetRenderWindow(const sf::Window &window, int width, int height);
+    sf::Vector2i screenToField(int width, int height);
+    void loadSpritesheet();
 };
--- src/rendered.cpp
+++ src/rendered.cpp
@@ -2,14 +2,30 @@
 #include <SFML/Graphics.hpp>
 #include "renderer/renderer.hpp"
 
-Renderer::Renderer() {
+void FieldDisplay::setSpritesheet(std::string texPath) {
+    sf::Texture tex(texPath);
+    spritesheet = tex;
+}
+
+void Renderer::loadSpritesheet() {
+    fieldDisplay.setSpritesheet("textures/minesweeperSpritesheet.png");
+}
+
+Renderer::Renderer(int w, int h) : windowW(w), windowH(h) {
 
 }
 
-void Renderer::renderField() {
-    
+void Renderer::renderField(sf::Window &window) {
+
 }
 
-void Renderer::fieldToScreen() {
+void Renderer::resetRenderWindow(const sf::Window &window, int width, int height) {
+    sf::Vector2u windowDim = window.getSize();
+    windowW = windowDim.x;
+    windowH = windowDim.y;
+}
 
+sf::Vector2i Renderer::screenToField(int width, int height) {
+    int x, y; //tmp for illustration of usage, get screen pos, translate to interger coords, return
+    return sf::Vector2i{x,y};
 }
